a4dc5bf1b0cb248c3a8d145459e4b74f5ff4ec1d
Glebas <Glebas@tut.by>
2016-01-04 17:16:07 +0200

    Step 3. save articles

--- borrowers/app/controllers/articles/new.js
+++ borrowers/app/controllers/articles/new.js
@@ -1,33 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  //isValid: Ember.computed(
-  //  'model.description',
-  //  'model.notes',
-  //  {
-  //    get(){
-  //      console.log(this);
-  //      return !Ember.isEmpty(this.get('model.description')) &&
-  //        !Ember.isEmpty(this.get('model.notes'))
-  //        ;
-  //    }
-  //  }
-  //),
-  //actions: {
-  //  save(){
-  //    this.set('errorMessage','');
-  //    if (this.get('isValid')) {
-  //      this.get('model').save().then((article) => {
-  //        this.transitionToRoute('articles.show', article);
-  //      });
-  //    } else {
-  //      this.set('errorMessage','You have to fill all the fields of article');
-  //    }
-  //    return true;
-  //  },
-  //  cancel(){
-  //    return true;
-  //  }
-  //}
+  isValid: Ember.computed(
+    'model.description',
+    'model.notes',
+    {
+      get(){
+        console.log(this);
+        return !Ember.isEmpty(this.get('model.description')) &&
+          !Ember.isEmpty(this.get('model.notes'))
+          ;
+      }
+    }
+  ),
+  actions: {
+    save(){
+      this.set('errorMessage','');
+      if (this.get('isValid')) {
+        this.get('model').save().then(() => {
+          this.transitionToRoute('friends');
+        });
+      } else {
+        this.set('errorMessage','You have to fill all the fields of article');
+      }
+      return true;
+    },
+    cancel(){
+      return true;
+    }
+  }
 
 });
